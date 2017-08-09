75ae7a345374ce42f4fc5d67532cb80874262353
balaji740 <balaji740@gmail.com>
2017-08-09 21:21:24 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,11 @@ alert('hi its javascript');
 var img = document.getElementById('madi');
 var marginleft = 0;
 function moveRight() {
-    marginleft = marginleft + 10;
+    marginleft = marginleft + 1;
     img.style.marginLeft = marginleft + 'px';
 }
 img.onclick=function(){
   //console.log('Loaded-test');    
   //img.style.marginLeft='100px';  
-  var interval = (moveRight,100);
+  var interval = (moveRight,50);
 };
