e61dfcdd6b03c4ce081938b3429fc2a8b8f2419b
balaji740 <balaji740@gmail.com>
2017-08-10 20:36:51 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,19 @@
-
-alert('hi its javascript');
-var img = document.getElementById('madi');
-var marginleft = 0;
-function moveRight() {
-    marginleft = marginleft + 1;
-    img.style.marginLeft = marginleft + 'px';
-}
-img.onclick=function(){
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function(){
+  counter = counter + 1;     
+  var span = document.getElementById('count');
+  span.innerHtml = counter.toString();
+};
+//alert('hi its javascript');
+//var img = document.getElementById('madi');
+//var marginleft = 0;
+//function moveRight() {
+//    marginleft = marginleft + 1;
+//    img.style.marginLeft = marginleft + 'px';
+//}
+//img.onclick=function(){
   //console.log('Loaded-test');    
   //img.style.marginLeft='100px';  
-  var interval = setInterval(moveRight,50);
-};
+//  var interval = setInterval(moveRight,50);
+//};
