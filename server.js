e6f71bd613f7817be9145ec15fd6517eca7002fc
balaji740 <balaji740@gmail.com>
2017-08-10 19:48:25 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,6 +105,12 @@ function createTemplate(data){
 </html>`;
 return htmlTemplate;
 }
+
+var counter = 0;
+app.get('/counter',function (req,res){
+    counter = counter + 1;
+    res.send(counter,toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
