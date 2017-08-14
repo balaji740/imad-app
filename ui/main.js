631f3dd293a12d9c43437e9370d4a7cbd34225a0
balaji740 <balaji740@gmail.com>
2017-08-14 20:31:13 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,22 @@
 
 var button = document.getElementById('counter');
-var counter = 0;
+//var counter = 0;
 button.onclick = function(){
-  counter = counter + 1;     
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  //counter = counter + 1;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+  if (request.readyState === XMLHttpRequest.DONE){
+      if (request.status == 200){
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();        
+      }
+  }
+  
 };
+
+request.open('GET','http://balaji740.imad.hasura-app.io/counter',true);
+request.send(null);
 //alert('hi its javascript');
 //var img = document.getElementById('madi');
 //var marginleft = 0;
@@ -17,4 +28,4 @@ button.onclick = function(){
   //console.log('Loaded-test');    
   //img.style.marginLeft='100px';  
 //  var interval = setInterval(moveRight,50);
-//};
+};
