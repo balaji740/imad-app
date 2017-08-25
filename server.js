134c15565116caa05f61710f5aacd3a880a9087c
balaji740 <balaji740@gmail.com>
2017-08-25 21:04:36 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ function createTemplate(data){
            ${heading}
            </h3>
            <div>
-            ${date.toDateString()}
+            ${date}
            </div>
         
             <div>
