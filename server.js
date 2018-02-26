49b6a887b62d381725dba55a639fee37efde0118
balaji740 <balaji740@gmail.com>
2018-02-26 20:39:31 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.get('/test-db', function(req,res){
  //   res.send(createTemplate(articles[articleName]));
 
 //Pool.query("SELECT * FROM ariclea WHERE title='" + req.params.articleName + "'", function(err,result) {
-  Pool.query("SELECT * FROM ariclea WHERE title = $1",[req.params.articleName],function(err,result) { 
+  Pool.query("SELECT * FROM articlea WHERE title = $1",[req.params.articleName],function(err,result) { 
   
     if (err){
         res.status(500).send(err.toString());
