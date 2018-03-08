6cab71e1eb74f56115027dcc2fe81ca7ed4c0b97
balaji740 <balaji740@gmail.com>
2018-03-08 20:18:00 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,7 +158,8 @@ app.post('/create-user',function(req,res){
 app.post('/login',function(req,res){
      var username = req.body.username;
      var password = req.body.password;
-    
+     console.log('serverjs');
+     console.log(password);
     Pool.query('SELECT * FROM "user" where username= $1',[username], function(err, result){
         if(err){
             res.status(500).send(err.toString());
