c7c8689e7c3cf50f53fd669ee4f8dbcff6b76383
balaji740 <balaji740@gmail.com>
2018-03-08 19:56:28 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,30 +29,49 @@ request.send(null);
   //img.style.marginLeft='100px';  
 //  var interval = setInterval(moveRight,50);
 };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var button = document.getElementById('btn_id');
-button.onclick = function(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-    if (request.readyState === XMLHttpRequest.DONE){
-        if (request.status == 200) {
-            var list = " ";
+//var nameInput = document.getElementById('name');
+//var name = nameInput.value;
+//var button = document.getElementById('btn_id');
+//button.onclick = function(){
+//    var request = new XMLHttpRequest();
+//    request.onreadystatechange = function(){
+//    if (request.readyState === XMLHttpRequest.DONE){
+//        if (request.status == 200) {
+//            var list = " ";
   //          var names = ['name1','name2'];
-            var names = request.responseText;
-            names = JSON.parse(names);
-            for(var i=0; i<names.length;i++){
-             list += '<li>' + names[i] + '</li>';
-           }
-     var ui = document.getElementById('namelist');
-     ui.innerHTML = list;            
-    }
-  }
-    
+//            var names = request.responseText;
+//            names = JSON.parse(names);
+//            for(var i=0; i<names.length;i++){
+//             list += '<li>' + names[i] + '</li>';
+//           }
+//     var ui = document.getElementById('namelist');
+//     ui.innerHTML = list;            
+//    }
+//  }
+
+button.onClick = function(){
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+      if (request.readyState === XMLHttpRequest.DONE){
+          if (request.status == 200) {
+              alert(logged in successfully);
+          } else if (request.status == 403){
+              alert('username/password is incorrect');
+              
+          } else if (request.status ==500){
+              alert('server error');
+          }
+              
+          }
 };
-  var nameInput = document.getElementById('name');
-var name = nameInput.value;
-  request.open('GET', 'http://balaji740.imad.hasura-app.io/submit-name?name=' + name,true);
-  request.send(null);
+//var nameInput = document.getElementById('name');
+//var name = nameInput.value;
+//request.open('GET', 'http://balaji740.imad.hasura-app.io/submit-name?name=' + name,true);
+//  request.send(null);
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  request.open('POST', 'http://balaji740.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username:username,password:password}));
 }; 
     
