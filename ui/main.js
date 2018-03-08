8e6d4dabea6815b5b880b8ff738f573effa5ad16
balaji740 <balaji740@gmail.com>
2018-03-08 19:58:54 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -70,6 +70,8 @@ button.onClick = function(){
 //  request.send(null);
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
+  console.log('username');
+  console.log('password');
   request.open('POST', 'http://balaji740.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username:username,password:password}));
